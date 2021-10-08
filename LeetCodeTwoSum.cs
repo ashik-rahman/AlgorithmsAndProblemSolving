09c0ab8118270a2f2e25b276ee02610d4988fd24
ashik-rahman <sahkiur52@gmail.com>
2021-10-08 21:56:17 +0600

    Add project files.

---
+++ LeetCodeTwoSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndProblemSolving
+{
+    class LeetCodeTwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int targerRemains = target - nums[i];
+                int index = Array.IndexOf(nums, targerRemains);
+                if (index != -1 && index != i)
+                {
+                    return new int[] { i, index };
+                }
+            }
+            return null;
+
+        }
+    }
+}
