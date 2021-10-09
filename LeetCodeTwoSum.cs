4d199d2f13f983910f7678f7490d140e7a8b7be9
ashik-rahman <sahkiur52@gmail.com>
2021-10-09 23:47:14 +0600

    implimentation using hashtable

--- LeetCodeTwoSum.cs
+++ LeetCodeTwoSum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,25 @@ namespace AlgorithmsAndProblemSolving
                 int targerRemains = target - nums[i];
                 int index = Array.IndexOf(nums, targerRemains);
                 if (index != -1 && index != i)
-                {
+                { 
                     return new int[] { i, index };
+                }
+            }
+            return null;
+        }
+        public int[] TwoSumWithHashTable(int[] nums, int target)
+        {
+            //to get O(1) solution, need to use reverse hashtable -> key become value and value become key
+            var hashList = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int targerRemains = target - nums[i];
+                if(hashList.ContainsKey(targerRemains)){
+                    return new int[]{i,(int)hashList[targerRemains]}; //hashTable[key] returns object that's why conversion needed
+                }else{
+                    if(!hashList.ContainsKey(nums[i])){
+                        hashList[nums[i]]= i;
+                    }
                 }
             }
             return null;
@@ -24,7 +42,7 @@ namespace AlgorithmsAndProblemSolving
         public void CallAndPrint(int[] nums, int target)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            var results = TwoSum(nums, target);
+            var results = TwoSumWithHashTable(nums, target);
             watch.Stop();
             Console.WriteLine($"1. Two Sums\n" +
                               $"Input Array = {this.printInputArray(nums)} target = {target}\n" +
